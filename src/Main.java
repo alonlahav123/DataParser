5b04a7a085c13470cff9e6efd9ab57d4b6ea6617
alonlahav123 <alonlahav123@gmail.com>
2019-03-01 19:37:18 -0800

    add test of file reading to main meathod

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,10 @@
  * @author Alon Lahav
  */
 public class Main {
+    public static void main(String[] args) {
+        // test of utils
+
+        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        System.out.println(data);
+    }
 }
