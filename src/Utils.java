456b7d62d081b1274bb343a9a1a528fb6b467bc9
alonlahav123 <alonlahav123@gmail.com>
2019-03-03 21:00:53 -0800

    Finish parse2016ElectionResults on Util

--- src/Utils.java
+++ src/Utils.java
@@ -23,5 +23,14 @@ public class Utils {
 
     public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
         ArrayList<ElectionResult> results = new ArrayList<>();
+        String[] lines = data.split("\n");
+
+        for(int i = 0; i < lines.length; i++) {
+            String[] d = data.split(",");
+            ElectionResult e = new ElectionResult(d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9], d[10]);
+            results.add(e);
+        }
+
+        return results;
     }
 }
