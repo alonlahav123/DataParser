4f6531527d0819e9546cdb19b28968681cf08009
alonlahav123 <alonlahav123@gmail.com>
2019-03-03 23:18:41 -0800

    fixed parse2016ElectionData

--- src/Utils.java
+++ src/Utils.java
@@ -26,11 +26,25 @@ public class Utils {
         String[] lines = data.split("\n");
 
         for(int i = 0; i < lines.length; i++) {
-            String[] d = data.split(",");
-            ElectionResult e = new ElectionResult(d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9], d[10]);
-            results.add(e);
+            int commaPlace = outOfPlaceComma(lines[i]);
+            String currentLine = lines[i].substring(2,commaPlace) + lines[i].substring(commaPlace+1);
+
+            String[] d = currentLine.split(",");
+            ElectionResult New = new ElectionResult(d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9]);
+            results.add(New);
         }
 
         return results;
+    }
+
+    private static int outOfPlaceComma(String line) {
+
+        for(int j = 0; j < line.length(); j++) {
+            if(line.substring(j,j+1).equals("\"")) {
+                return line.indexOf(",", j);
+            }
+        }
+
+        return -1;
     }
 }
