f8065e885e560cb3687204c43c61e1299b8fc045
alonlahav123 <alonlahav123@gmail.com>
2019-03-05 18:59:09 -0800

    got algorithim to work

--- src/Utils.java
+++ src/Utils.java
@@ -25,26 +25,44 @@ public class Utils {
         ArrayList<ElectionResult> results = new ArrayList<>();
         String[] lines = data.split("\n");
 
-        for(int i = 0; i < lines.length; i++) {
-            int commaPlace = outOfPlaceComma(lines[i]);
-            String currentLine = lines[i].substring(2,commaPlace) + lines[i].substring(commaPlace+1);
+        for(int i = 1; i < lines.length; i++) {
+            String line = lines[i];
+            ElectionResult d;
 
-            String[] d = currentLine.split(",");
-            ElectionResult New = new ElectionResult(d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9]);
-            results.add(New);
+            if(line.indexOf("\"") == -1 ) {
+                d = getDataPoint(line);
+            } else {
+                String newLine = removeQuotations(line);
+                d = getDataPoint(newLine);
+            }
+
+            results.add(d);
         }
 
         return results;
     }
 
-    private static int outOfPlaceComma(String line) {
+    private static String removeQuotations(String line) {
+        int start = line.indexOf("\"");
+        int end = line.indexOf( "\"" , start+1);
+        String num = "";
 
-        for(int j = 0; j < line.length(); j++) {
-            if(line.substring(j,j+1).equals("\"")) {
-                return line.indexOf(",", j);
+        for(int i = start; i < end; i++) {
+            String letter = line.substring(i, i+1);
+
+            if(!letter.equals("\"") && !letter.equals(",")) {
+                num += letter;
             }
         }
 
-        return -1;
+        String data = line.substring(0, start) + num + line.substring(end+1);
+
+        return data;
     }
+
+    private static ElectionResult getDataPoint(String line) {
+        String[] d = line.split(",");
+        return new ElectionResult(d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9], d[10]);
+    }
+
 }
