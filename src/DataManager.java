275505baad9f691967a6b587dad5e9580f3e0ef6
alonlahav123 <alonlahav123@gmail.com>
2019-03-11 01:27:17 -0700

    started separating data

--- src/DataManager.java
+++ src/DataManager.java
@@ -4,7 +4,15 @@ import java.util.List;
 public class DataManager {
     private ArrayList<State> states;
 
-    public  DataManager() { }
+    public  DataManager(ArrayList<ElectionResult> elecResults, ArrayList<EducationResult> educResults, ArrayList<UnemploymentResult> unempResults) {
+        for(int i = 0; i < elecResults.size(); i++) {
+            String state = elecResults.get(i).getState_abbr();
+
+            if(states.indexOf(state) == -1) {
+
+            }
+        }
+    }
 
     public ArrayList<State> getStates() {
         return states;
